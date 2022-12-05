4159b5a75311861e3a5cf1667baf5bc3194a9d18
Xerx350 <andreychashin@outlook.com>
2022-12-05 14:41:30 +0300

    int cs

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -6,6 +6,7 @@ import io.github.humbleui.skija.Canvas;
 import io.github.humbleui.skija.Paint;
 import io.github.humbleui.skija.RRect;
 import io.github.humbleui.skija.Surface;
+import misc.CoordinateSystem2i;
 import misc.Misc;
 
 import java.io.File;
@@ -67,32 +68,24 @@ public class Application implements Consumer<Event> {
     /**
      * Рисование
      *
-     * @param canvas низкоуровневый инструмент рисования примитивов от Skija
-     * @param height высота окна
-     * @param width  ширина окна
+     * @param canvas   низкоуровневый инструмент рисования примитивов от Skija
+     * @param windowCS СК окна
      */
-    public void paint(Canvas canvas, int height, int width) {
+    public void paint(Canvas canvas, CoordinateSystem2i windowCS) {
         // запоминаем изменения (пока что там просто заливка цветом)
         canvas.save();
         // очищаем канвас
         canvas.clear(APP_BACKGROUND_COLOR);
-
-        // координаты левого верхнего края окна
-        int rX = width / 3;
-        int rY = height / 3;
-        // ширина и высота
-        int rWidth = width / 3;
-        int rHeight = height / 3;
         // создаём кисть
         Paint paint = new Paint();
         // задаём цвет рисования
         paint.setColor(Misc.getColor(100, 255, 255, 255));
         // рисуем квадрат
-        canvas.drawRRect(RRect.makeXYWH(rX, rY, rWidth, rHeight, 4), paint);
-
+        canvas.drawRRect(windowCS.getRRect(4), paint);
         // восстанавливаем состояние канваса
         canvas.restore();
     }
+
 
     /**
      * Обработчик событий
@@ -109,7 +102,7 @@ public class Application implements Consumer<Event> {
             window.close();
         } else if (e instanceof EventFrameSkija ee) {
             Surface s = ee.getSurface();
-            paint(s.getCanvas(), s.getWidth(), s.getHeight());
+            paint(s.getCanvas(), new CoordinateSystem2i(s.getWidth(), s.getHeight()));
         }
     }
 
